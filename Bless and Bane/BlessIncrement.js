f21aa25ff924eb9c58a1f4d517309b8bfbe2f935
Syven88 <104026611+Syven88@users.noreply.github.com>
2022-05-19 10:27:12 -0400

    Properly checks for bless on the selected token.

--- Bless and Bane/BlessIncrement.js
+++ Bless and Bane/BlessIncrement.js
@@ -1,18 +1,21 @@
 let caster = canvas.tokens.controlled[0]
-let blessAnim = Sequencer.EffectManager.getEffects({origin:"blessAnim",token:caster})[0] //locates the bless animation created by Bless_Animation
+let blessAnim = Sequencer.EffectManager.getEffects({origin:"blessAnim",source:caster})[0] //locates the bless animation created by Bless_Animation
 let incrementValue = 3; //3 is the value for radius = 1 square
 
 main()
 
 function main(){  
 //checks to ensure a single token is selected
-  if (canvas.tokens.controlled.length === 0){
+  if (canvas.tokens.controlled.length == 0){
   ui.notifications.error("Select the token on which a bless effect is active.")
+  return
+}else if(canvas.tokens.controlled.length > 1){
+  ui.notifications.error("Select only one token.")
   return
 }
 
 //checks to make sure bless is active on the selected token
-if (Sequencer.EffectManager.getEffects({origin:"blessAnim",token:caster})[0] === undefined){
+if (Sequencer.EffectManager.getEffects({origin:"blessAnim",source:caster})[0] === undefined){
   ui.notifications.error(`There are no instances of the bless effect active on this token.`)
   return
 }
