27c1ae9ca05d54356316a89992c6a06b1e3fe106
Syven88 <104026611+Syven88@users.noreply.github.com>
2022-05-19 10:30:09 -0400

    Properly checks for Bane on the selected token

--- Bless and Bane/BaneIncrement.js
+++ Bless and Bane/BaneIncrement.js
@@ -1,19 +1,22 @@
 let caster = canvas.tokens.controlled[0]
-let baneAnim = Sequencer.EffectManager.getEffects({origin:"baneAnim",token:caster})[0] //locates the bane animation created by bane_Animation
+let baneAnim = Sequencer.EffectManager.getEffects({origin:"baneAnim",source:caster})[0] //locates the bane animation created by bane_Animation
 let incrementValue = 3; //3 is the value for radius = 1 square
 
 main()
 
 function main(){  
 //checks to ensure a single token is selected
-  if (canvas.tokens.controlled.length === 0){
+  if (canvas.tokens.controlled.length == 0){
   ui.notifications.error("Select the token on which a bane effect is active.")
   return
-}
+}else if (canvas.tokens.controlled.length > 1){
+  ui.notifications.error("Select only one token with bless active.")
+  return
+} 
 
 //checks to make sure bane is active on the selected token
-if (Sequencer.EffectManager.getEffects({origin:"baneAnim",token:caster})[0] === undefined){
-  ui.notifications.error(`There are no instances of the bane effect active on this token.`)
+if (Sequencer.EffectManager.getEffects({origin:"baneAnim",source:caster})[0] === undefined){
+  ui.notifications.error("There are no instances of the bane effect active on this token.")
   return
 }
 
