a7b66032459a12eb61152bb492d7b523da19ee22
Syven88 <104026611+Syven88@users.noreply.github.com>
2022-07-22 10:37:37 -0400

    Added compatibility with free JB2A; Added Staff of healing and healing hands support

--- Heal and Harm/healAnimation.js
+++ Heal and Harm/healAnimation.js
@@ -1,8 +1,61 @@
 //vardef
 let caster = canvas.tokens.controlled[0]
 let spellTarget = Array.from(game.user.targets)[0]
+let actionsUsed
+let healingDie
+let spellTargetName = ``
 
-let d = new Dialog({
+let healingSingleAnim
+let healingDamageAnim
+let healingAreaAnim = "jb2a.healing_generic.burst.greenorange"
+let healingStrandAnim
+
+if(game.modules.get("jb2a_patreon")?.active){
+    healingSingleAnim = "jb2a.explosion.05.greenorange"
+    healingStrandAnim = "jb2a.energy_strands.range.standard.dark_green"
+    healingDamageAnim = "jb2a.divine_smite.target.greenyellow"
+}else if(!game.modules.get("jb2a_patreon")?.active && game.modules.get("JB2A_DnD5e")?.active){
+    console.log("The jb2a patreon module is not enabled!")
+    healingSingleAnim = "jb2a.healing_generic.200px.green"
+    healingStrandAnim = "jb2a.energy_strands.range.standard.purple"
+    healingDamageAnim = "jb2a.divine_smite.target.blueyellow"
+}else{
+    ui.notifications.error("The jb2a module is not present or enabled!")
+    return
+}
+
+let healBonus = 0
+
+async function main(){
+    if(canvas.tokens.controlled.length != 0){
+        if(CheckFeat(`healing-hands`)){
+            healingDie = `d10`
+        }else{
+            healingDie = `d8`
+        }
+        
+        if(CheckItem(`Staff of Healing`)){
+            healBonus = 1
+        }else if(CheckItem(`Staff of Healing (Greater)`)){
+            healBonus = 2
+        }else if(CheckItem(`Staff of Healing (Major)`)){
+            healBonus = 3
+        }else if(CheckItem(`Staff of Healing (True)`)){
+            healBonus = 4
+        }
+
+        if (!caster.actor.itemTypes.spell.some(s => s.slug === 'heal')){
+            ui.notifications.error(`This character can't cast spells or doesn't have heal prepared.`)
+            return
+        }
+        healActions.render(true);
+    }else{
+        ui.notifications.error(`You must select the token of the caster!`)
+        return;
+    }
+}
+
+let healActions = new Dialog({
     title: "Heal",
     content: "<p>How many actions are you using to cast Heal?</p>",
     buttons: {
@@ -31,52 +84,103 @@ let d = new Dialog({
     default: "Two Actions"
 })
 
-main()
+let rollFormula = ``    
+let spellNumeral
 
-function main(){
-    if(caster != undefined){
-        d.render(true);
-    }else{
-        ui.notifications.error(`You must select the token of the caster!`)
-        return;
-    }    
+function rollTheDice(){
+    console.log(`Actions used: `+ actionsUsed);
+
+    if(actionsUsed == 1 || actionsUsed == 3){
+        rollFormula = spellNumeral + healingDie + `+`+ healBonus
+    }else if(actionsUsed == 2 && healBonus != 0){
+        rollFormula = spellNumeral + healingDie + `+` + spellNumeral*8 + `+`+ healBonus
+    }else if(actionsUsed == 2 && healBonus == 0){
+        rollFormula = spellNumeral + healingDie + `+` + spellNumeral*8
+    }else{console.log(`Error in rollTheDice function!`);}    
+    console.log(`Roll formula: ` + rollFormula);
+    ui.chat.processMessage(`/r `+rollFormula+` #`+actionsUsed+` action Heal at `+selectedSpellLevel+` targeting `+spellTargetName)
 }
 
-//Defining unique behavior for each kind of effect
+const spellLevels = [`10th Level`, `9th Level`,`8th Level`,`7th Level`,`6th Level`,`5th Level`,`4th Level`,`3rd Level`,`2nd Level`,`1st Level`];
+const spLevelOptions = spellLevels.slice(-Math.ceil(caster?.actor.level/2)).reduce((acc, e) => acc += `<option value="${e}">${e}</option>`, ``)
+const content = `
+<form> 
+    <div class="form-group"> 
+        <label for="spellLevel-select">Spell Level</label>
+            <div class="form-fields">
+                <select id="spellLevel-select">${spLevelOptions}</select> 
+            </div> 
+        </div> 
+    </form>`
 
+let selectedSpellLevel
+
+let healLevel = new Dialog({
+    content,
+    title: "Select Spell Level",
+    buttons: {go: {
+        icon: `<i class="fas fa-check"></i>`,
+        label: `Confirm`,
+        callback: async (html) => {
+                selectedSpellLevel = html[0].querySelector("select[id='spellLevel-select']").value;
+                spellNumeral = 10-spellLevels.indexOf(selectedSpellLevel)
+                console.log(`Spell Level: `+spellNumeral);
+                rollTheDice()
+            }
+        },
+        cancel: {
+            icon: '<i class="fas fa-times"></i>',
+            label: `Cancel`
+        }
+    },
+    default: "go"
+})
+//Defining unique behavior for each kind of effect
 
 function oneActionHeal(){
     //error checking for multiple targets
-    if (game.user.targets.size!= 1){
+    if (game.user.targets.size > 1){
         ui.notifications.error("Select only one target!")
+        return;
+    }else if(game.user.targets.size == 0){
+        ui.notifications.error(`Please select a target!`)
         return;
     }
     if(canvas.grid.measureDistance(caster,spellTarget) < 7.5){
             console.log("Close enough to touch!")
             // checks whether the target is undead or not
             oneActionHealAnimation()
+            actionsUsed = 1;
         }else{
             console.log(`Too far to touch!`); 
             ui.notifications.error(`Your target is further than touch distance away! Try again.`);
             return;
-        }    
+        }
+        spellTargetName = spellTarget.name
+        healLevel.render(true)
 }
 
 function twoActionHeal(){
     //error checking for multiple targets
-    if (game.user.targets.size!= 1){
+    if (game.user.targets.size > 1){
         ui.notifications.error("Select only one target!")
+        return;
+    }else if(game.user.targets.size == 0){
+        ui.notifications.error(`Please select a target!`)
         return;
     }
     if(canvas.grid.measureDistance(caster,spellTarget) <= 30){
         console.log("Within 30 feet!")
         // checks whether the target is undead or not
         twoActionHealAnimation()
+        actionsUsed = 2;
     }else{
         console.log(`Farther than 30 feet!`); 
         ui.notifications.error(`Your target is further than 30 feet away! Try again.`);
         return;
     }
+    spellTargetName = spellTarget.name
+    healLevel.render(true)
 }
 
 function threeActionHeal(){
@@ -86,6 +190,8 @@ function threeActionHeal(){
     let tokenTraitsInRange = tokensInRange.map(x => x.actor.traits)
     let undeadTokensInRange = []
     let livingTokensInRange = []
+    spellTargetName = `all creatures within 30 feet.`
+    actionsUsed = 3
     for(let i=0;i<tokensInRange.length;i++){
         if(Array.from(tokenTraitsInRange[i]).includes(`undead`)){
             undeadTokensInRange.push(tokensInRange[i])
@@ -100,7 +206,7 @@ function threeActionHeal(){
             .atLocation(caster)
             .randomRotation()
             .size(15, {gridUnits: true})
-            .file("jb2a.healing_generic.burst.greenorange")
+            .file(healingAreaAnim)
             .waitUntilFinished(-1500)
         .play()
     for(let i=0;i<undeadTokensInRange.length;i++){
@@ -109,7 +215,7 @@ function threeActionHeal(){
                 .atLocation(undeadTokensInRange[i])
                 .randomRotation()
                 .scaleToObject(1.25)
-                .file("jb2a.divine_smite.target.greenyellow")
+                .file(healingDamageAnim)
             .play()
     }
     for(let i=0;i<livingTokensInRange.length;i++){
@@ -118,30 +224,31 @@ function threeActionHeal(){
                 .atLocation(livingTokensInRange[i])
                 .randomRotation()
                 .scaleToObject(1.25)
-                .file("jb2a.explosion.05.greenorange")
+                .file(healingSingleAnim)
             .play()
     }
+    healLevel.render(true)
 }
 
 //Defining animations for each kind of effect
     function oneActionHealAnimation(){
         if(Array.from(spellTarget.actor.traits).includes(`undead`)){
-        //trigger energy strand from caster to target, then trigger healing burst on target
+        //trigger energy strand from caster to target, then trigger damaging burst on target
         new Sequence("Damage undead")
             .effect()
                 .atLocation(spellTarget)
                 .randomRotation()
                 .scaleToObject(1.25)
-                .file("jb2a.divine_smite.target.greenyellow")
+                .file(healingDamageAnim)
             .play()
         }else{
-        //trigger energy strand from caster to target, then trigger damaging burst on target
+        //trigger energy strand from caster to target, then trigger healing burst on target
         new Sequence("Heal living")
             .effect()
                 .atLocation(spellTarget)
                 .randomRotation()
                 .scaleToObject(1.25)
-                .file("jb2a.explosion.05.greenorange")
+                .file(healingSingleAnim)
             .play()
         }
     }
@@ -152,13 +259,13 @@ function threeActionHeal(){
                 .effect()
                     .atLocation(caster)
                     .stretchTo(spellTarget)
-                    .file("jb2a.energy_strands.range.standard.dark_green")
+                    .file(healingStrandAnim)
                     .waitUntilFinished(-1000)
                 .effect()
                     .atLocation(spellTarget)
                     .randomRotation()
                     .scaleToObject(1.25)
-                    .file("jb2a.divine_smite.target.greenyellow")
+                    .file(healingDamageAnim)
                 .play()
         }else{
             //trigger energy strand from caster to target, then trigger damaging burst on target
@@ -166,13 +273,29 @@ function threeActionHeal(){
                 .effect()
                     .atLocation(caster)
                     .stretchTo(spellTarget)
-                    .file("jb2a.energy_strands.range.standard.dark_green")
+                    .file(healingStrandAnim)
                     .waitUntilFinished(-1000)
                 .effect()
                     .atLocation(spellTarget)
                     .randomRotation()
                     .scaleToObject(1.25)
-                    .file("jb2a.explosion.05.greenorange")
+                    .file(healingSingleAnim)
                 .play()
         }
     }
+
+function CheckFeat(slug) {
+    if (caster.actor.itemTypes.feat.find((i) => i.slug === slug)) {
+        return true;
+    }
+    return false;
+    }
+
+function CheckItem(slug) {
+    if (caster.actor.inventory.find((i) => i.data.name === slug)) {
+        return true;
+    }
+    return false;
+    }
+
+main()
